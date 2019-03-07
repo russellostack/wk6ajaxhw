80cbefa13a60e6fc66619dc7ced310bac05e998a
Russell Stack <russellostack@gmail.com>
2019-03-07 10:50:17 -0500

    css started, titles added to gifs from js, some comments added, small js fixes like spelling movies incorrectly a million times, data-animate still confuses me and i cant get it to be defaulted to gifs instead of images. repeat button checking added

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,6 @@
 var movies = ["Firefly", "cars", "Finding Nemo", "Transformers", "What We Do in the Shadows", "Monty Python's the Holy Grail,"];
 
+//function that takes movie array and adds buttons to the html initialized with a data-movie attribute.
 function buttonMaker() {
     $("#gif-div").empty();
     for (var i = 0; i < movies.length; i++) {
@@ -7,9 +8,12 @@ function buttonMaker() {
         newButton.addClass("btn btn-success movie-button");
         newButton.attr("data-movie", movies[i]);
         newButton.text(movies[i]);
-        $("#gif-div").append(newButton);
+        $("#buttons").append(newButton);
     };
 };
+
+//event listener for each gif button made, encompases ajax call to giphy api as well as pushing the results to html. 
+// Do i need to keep the gifs from previous searches?
 $(document).on("click", ".movie-button", function () {
     $("#gif-div").empty();
     var newMovie = $(this).attr("data-movie");
@@ -20,7 +24,9 @@ $(document).on("click", ".movie-button", function () {
         method: "GET"
     })
         .then(function (response) {
+            // console.log(this);
             var giphyObj = response.data;
+            console.log(giphyObj);
             for (var i = 0; i < giphyObj.length; i++) {
 
                 if (giphyObj[i].rating !== "r") {
@@ -30,27 +36,33 @@ $(document).on("click", ".movie-button", function () {
                     var rating = giphyObj[i].rating;
                     var p = $("<p>").text("Rating: " + rating);
                     var gifImg = $("<img>");
-                    gifImg.attr("src", giphyObj[i].images.fixed_width_still.url)
-                    gifImg.attr("data-still", giphyObj[i].images.fixed_width_still.url)
-                    gifImg.attr("data-animate", giphyObj[i].images.fixed_width.url)
-                    gifImg.attr("data-state", "still")
+                    gifImg.attr("src", giphyObj[i].images.fixed_width_still.url);
+                    gifImg.attr("data-still", giphyObj[i].images.fixed_width_still.url);
+                    gifImg.attr("data-animate", giphyObj[i].images.fixed_width.url);
+                    gifImg.attr("title", giphyObj[i].title);
+                    // If i change this attribute to "animate" why does it not initialize every gif as an animated gif?
+                    gifImg.attr("data-state", "still");
                     gifImg.addClass("gif");
                     newGifDiv.append(gifImg);
-                    newGifDiv.append(p);
+                    newGifDiv.prepend(p);
                     $("#gif-div").append(newGifDiv);
                 }
             }
         })
 });
+
+// movie submit event listener, adds movie from input to movie array then reruns the button maker.
 $("#movie-button").on("click", function (event) {
     event.preventDefault();
     var movie = $("#movie-input").val().trim();
-    if (movie === "") {
+    if (movie === "" || $.inArray(movie, movies !== -1)) {
         return
     }
     movies.push(movie);
     buttonMaker();
 });
+
+// gif start and stop function event listener, starts with still gifs.
 $(document).on("click", ".gif", function () {
 
     var state = $(this).attr("data-state");
@@ -62,4 +74,5 @@ $(document).on("click", ".gif", function () {
         $(this).attr("data-state", "still");
     }
 });
+//runs the button maker function on page load with initial array.
 buttonMaker();
