fd2d49fed1e93bc8119f1eee083a0b5ede84b2cf
Russell Stack <russellostack@gmail.com>
2019-03-07 09:43:36 -0500

    js works, ajax connects and displays gifs, html updates well, need to do css on buttons and newly created gif divs

--- javascript/app.js
+++ javascript/app.js
@@ -1,31 +1,65 @@
-var movies = ["Firefly", "cars", "Finding Nemo", "Transformers", "What We Do in the Shadows", "Monty Python's the Holy Grail"];
+var movies = ["Firefly", "cars", "Finding Nemo", "Transformers", "What We Do in the Shadows", "Monty Python's the Holy Grail,"];
 
-function buttonMaker(){
-    for (var i=0; i < movies.length; i++){
-    var newButton = $("#button");
-    newButton.addClass("btn btn-success");
-    newButton.attr("data-movie",movies[i]);
-    newButton.text(movies[i]);
-    $("#gif-div").append(newButton);
+function buttonMaker() {
+    $("#gif-div").empty();
+    for (var i = 0; i < movies.length; i++) {
+        var newButton = $("<button>");
+        newButton.addClass("btn btn-success movie-button");
+        newButton.attr("data-movie", movies[i]);
+        newButton.text(movies[i]);
+        $("#gif-div").append(newButton);
     };
-    $(".movie-button").on("click", function(){
-        $("#gifs").remove();
-        var newMovie = $(this).attr("data-movie");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newMovie + "&api_key=OKuVGjgT0V7r20tpzmToIqOjeggIVUYM&limit=10";
-        $.ajax({
-            url: queryURL,
-            method:"GET"
-        })
-        .then(function(response){
+};
+$(document).on("click", ".movie-button", function () {
+    $("#gif-div").empty();
+    var newMovie = $(this).attr("data-movie");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + newMovie + "&api_key=idUSKX9OnepbhgPb8StIaa3e7IpaT2lq&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
             var giphyObj = response.data;
-            for (vari=0;i<giphyObj.length;i++){
-                if (giphyObj[i].rating !== "r"){
+            for (var i = 0; i < giphyObj.length; i++) {
+
+                if (giphyObj[i].rating !== "r") {
+
                     var newGifDiv = $("<div>");
-                    newGifDiv.addClas("gifs");
+                    newGifDiv.addClass("gifs");
                     var rating = giphyObj[i].rating;
-                    
+                    var p = $("<p>").text("Rating: " + rating);
+                    var gifImg = $("<img>");
+                    gifImg.attr("src", giphyObj[i].images.fixed_width_still.url)
+                    gifImg.attr("data-still", giphyObj[i].images.fixed_width_still.url)
+                    gifImg.attr("data-animate", giphyObj[i].images.fixed_width.url)
+                    gifImg.attr("data-state", "still")
+                    gifImg.addClass("gif");
+                    newGifDiv.append(gifImg);
+                    newGifDiv.append(p);
+                    $("#gif-div").append(newGifDiv);
                 }
             }
         })
-    })
-};
+});
+$("#movie-button").on("click", function (event) {
+    event.preventDefault();
+    var movie = $("#movie-input").val().trim();
+    if (movie === "") {
+        return
+    }
+    movies.push(movie);
+    buttonMaker();
+});
+$(document).on("click", ".gif", function () {
+
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+});
+buttonMaker();
